bde7b0dff20361f2487111cd898930532bcf7848
shivsundar3009 <shivsundar3009@gmail.com>
2023-10-04 23:30:22 +0530

    first function in dsa journey

---
+++ Practice.js
@@ -0,0 +1,7 @@
+// write a function that checks if given number is a multiple of both 3 and 5
+
+isNumMultipleOf3And5 = (num)=>{
+    return num % 15 === 0
+}
+
+console.log(isNumMultipleOf3And5(25));
