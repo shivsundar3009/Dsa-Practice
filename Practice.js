92096376ba3b1c379e0aec52a60fd8bd7aa58e3e
shivsundar3009 <shivsundar3009@gmail.com>
2023-10-05 23:47:54 +0530

    second practice question

--- Practice.js
+++ Practice.js
@@ -5,3 +5,23 @@ isNumMultipleOf3And5 = (num)=>{
 }
 
 console.log(isNumMultipleOf3And5(25));
+
+
+// write a function that gives sum of n numbers from 1
+
+function sumOfFirstNNumbers(n) {
+    if (n <= 0) {
+      return 0;
+    }
+  
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+      sum += i;
+    }
+    return sum;
+  }
+  
+  // Example usage:
+  console.log(sumOfFirstNNumbers(5)); // 15 (1 + 2 + 3 + 4 + 5)
+  console.log(sumOfFirstNNumbers(10)); // 55 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10)
+  
