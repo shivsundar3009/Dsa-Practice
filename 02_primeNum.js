83d90fba9c4e521306871658061cfbd6d5d895c2
shivsundar wakle <126707206+shivsundar3009@users.noreply.github.com>
2024-01-28 23:37:03 +0530

    Create 02_primeNum.js

---
+++ 02_primeNum.js
@@ -0,0 +1,14 @@
+function isPrime(number) {
+  if (number <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Example usage:
+
+console.log(isPrime(7)); // Output: true
+console.log(isPrime(12)); // Output: false
